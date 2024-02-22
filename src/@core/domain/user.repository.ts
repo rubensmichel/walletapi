5f7992aa3b403ce8b6d3cc6c65a409b136337dd7
rubensmichel <michelrubens.7@gmail.com>
2024-02-21 21:24:25 -0300

    chore: remove unused files

--- src/@core/domain/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './user.entity';
-
-export interface UserRepositoryInterface {
-  insert(user: User): Promise<void>;
-  findAll(): Promise<User[]>;
-}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,13 @@
+type CreateUserInput = {
+    name: string;
+    document: string;
+    email: string;
+    password: string;
+};
+
+type CreateUserOutput = {
+    name: string;
+    document: string;
+    email: string;
+    password: string;
+};
