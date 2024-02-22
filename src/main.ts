54c0f5756b36d55f324782207e60ab414e0d59bd
rubensmichel <michelrubens.7@gmail.com>
2024-02-21 23:11:19 -0300

    chore: exaple documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+  const options = new DocumentBuilder()
+    .setTitle("Wallet api")
+    .setDescription("My Wallet API documentation")
+    .setVersion("1.0")
+    .addTag("user")
+    .build();
+
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+    @ApiProperty({
+        example: "rubens",
+    })
+    name: string;
+    @ApiProperty({
+        example: "4745876523",
+    })
+    document: string;
+    @ApiProperty({
+        example: "rubens@test.com",
+    })
+    email: string;
+    @ApiProperty({
+        example: "123456789",
+    })
+    password: string;
+}
