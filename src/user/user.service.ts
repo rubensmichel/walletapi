b7044c361c5a527948f1ea6bd3957022cf386f04
rubensmichel <michelrubens.7@gmail.com>
2024-02-22 13:38:42 -0300

    chore:

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,19 +2,18 @@ import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from './repository/repository';
 import { User } from './entities/user.entity';
-import { Logger } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
   constructor(@Inject('Repository') private usersRepository: Repository<User>) {}
 
   create(createUserDto: CreateUserDto) {
-    let user = this.usersRepository.getByDocument(createUserDto.document)
+    let user = this.usersRepository.getByDocument(createUserDto.document);
     if (user != null){
       throw new BadRequestException("user with this document already create");
     }
 
-    user = this.usersRepository.getByEmail(createUserDto.email)
+    user = this.usersRepository.getByEmail(createUserDto.email);
     if (user != null){
       throw new BadRequestException("user with this email already create");
     }
