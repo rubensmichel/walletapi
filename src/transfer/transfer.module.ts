03cdb93f01ed78f3d06b5518e49efafb1cd52b25
rubensmichel <michelrubens.7@gmail.com>
2024-02-23 02:36:47 -0300

    chore: rollback in case error

--- src/transfer/transfer.module.ts
+++ src/transfer/transfer.module.ts
@@ -3,9 +3,10 @@ import { TransferService } from './transfer.service';
 import { TransferController } from './transfer.controller';
 import { TransferRepository } from './repository/transfer.repository';
 import { WalletModule } from 'src/wallet/wallet.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [WalletModule],
+  imports: [WalletModule, HttpModule],
   controllers: [TransferController],
   providers: [TransferService, {
     provide: 'Repository',
--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -1,17 +1,20 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { TransferDto } from './dto/transfer.dto';
 import { WalletService } from 'src/wallet/wallet.service';
 import { Repository } from './repository/repository';
 import { Transfer } from './entities/transfer.entity';
+import { Observable } from 'rxjs';
+import axios, { AxiosResponse } from 'axios';
 
 @Injectable()
 export class TransferService {
+    httpService: any;
     constructor(@Inject('Repository') private transferRepository: Repository<Transfer>) {}
 
     @Inject(WalletService)
     private readonly walletService: WalletService;
 
-    transfer(transferDto: TransferDto) {
+    async transfer(transferDto: TransferDto) {
         let walletOrigin = this.walletService.findOne(transferDto.walletOriginId)
         if (walletOrigin == null) {
             throw new NotFoundException("wallet origin id not found");
@@ -22,7 +25,27 @@ export class TransferService {
             throw new NotFoundException("wallet destination id not found");
         }
 
+        if (transferDto.value > walletOrigin.balance){
+            throw new ConflictException("no have balance to complete this transfer");
+        }
+
         this.walletService.debit(walletOrigin, transferDto.value)
+
+        let statusCode = await axios.post('https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc',
+        {
+          headers: {
+            'Content-Type': 'application/json',
+        },
+        }).then((res) => {
+            return res.status;
+        });
+
+        if (statusCode != 201){
+            Logger.log("unauthorized transfer, executing rollback")
+            this.walletService.credit(walletOrigin, transferDto.value)
+            throw new ConflictException("unauthorized transfer");
+        }
+        
         this.walletService.credit(walletDest, transferDto.value)
         this.transferRepository.create(transferDto)
 
